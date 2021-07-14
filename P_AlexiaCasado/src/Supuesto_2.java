c10bde5e7a38998c481cd1c5a4a7336ebf751c16
acasag03 <acasag03@estudiantes.unileon.es>
2021-07-14 10:37:45 +0200

    Nuevo Supuesto

---
+++ P_AlexiaCasado/src/Supuesto_2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+/**
+ * @author Alexia Casado Gonzalez
+ * @version 1.0
+ *
+ */
+public class Supuesto_2 
+{
+	//CONSTRUCTOR
+	public Supuesto_2()
+	{
+		
+	}
+	
+	public void Ejercicio_2()
+	{
+		
+	}
+}
