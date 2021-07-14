e0ba8799582bfe9917ef7b1379a9ac1a2a483db8
acasag03 <acasag03@estudiantes.unileon.es>
2021-07-14 12:12:11 +0200

    Finalizada el supuesto_2

--- P_AlexiaCasado/src/Supuesto_2.java
+++ P_AlexiaCasado/src/Supuesto_2.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
  */
 /**
  * @author Alexia Casado Gonzalez
- * @version 1.0
+ * @version 2.0
  *
  */
 public class Supuesto_2 
 {
+	double r = 1;
 	//CONSTRUCTOR
 	public Supuesto_2()
 	{
 		
 	}
+	
 	/**
-	 * 
+	 * ESTE METODO SE ENCARGARA DE LLAMAR A LOS DIFERENTES METODOS Y CONSEGUIR LOS NÚMEROS DE MANERA CORRECTA POR CONSOLA.
 	 */
 	public void Ejercicio_2()
 	{
@@ -59,7 +61,7 @@ public class Supuesto_2
 		double num1 = Double.parseDouble(entrada_1);
 		double num2 = Double.parseDouble(entrada_2);
 		int redondeo = Integer.parseInt(entrada_3);
-		double r = 1;
+
 		//En un principio se iba a utilizar un switch case, pero no se puede determinar redondeo que se introducido por pantalla.
 		//Dependiendo del número de redondeo generara un numero para realizar el redondeo, es decir, si el numero es 3 de redondeo r sera 1000 para que al multiplicar por el el numero este redondeado a 3 decimales.
 		for(int j = 0; j < redondeo ;j++)
@@ -71,6 +73,25 @@ public class Supuesto_2
 		double num1R = Math.round(num1 * r) / r;
 		double num2R = Math.round(num2 * r) / r;
 		
+		System.out.println("Los números redondeados: " + num1R + " y " + num2R);
+		
+		//Suma
+		sumaNum(num1R, num2R);
+		
+		//Resta
+		restaNum(num1R,num2R);
+		
+		//División
+		divisionNum(num1R,num2R);
+		
+		//Multiplicación
+		multiplicacionnNum(num1R,num2R);
+		
+		//Modulo
+		moduloNum(num1R,num2R);
+		
+		//Mayor que
+		mayorMenor(num1R,num2R);
 	}
 	
 	/**
@@ -115,5 +136,98 @@ public class Supuesto_2
 		
 	}
 	
+	/**
+	 * 
+	 * @param num1 : PRIMER NUMERO INTRODUCIDO POR CONSOLA
+	 * @param num2 : SEGUNDO NUMERO INTRODUCIDO POR CONSOLA
+	 */
+	public void sumaNum(double num1, double num2)
+	{
+		double suma = num1 + num2;
+		suma = Math.round(suma * r) / r;
+		
+		System.out.println("La suma de ambos números es: " + suma);
+	}
 	
+	/**
+	 * 
+	 * @param num1 : PRIMER NUMERO INTRODUCIDO POR CONSOLA
+	 * @param num2 : SEGUNDO NUMERO INTRODUCIDO POR CONSOLA
+	 */
+	public void restaNum(double num1, double num2)
+	{
+		//Resta de ambos numeros
+		double resta = num1 - num2;
+		resta = Math.round(resta * r) / r;
+		
+		System.out.println("La resta de "+ num1 + "-" + num2+ " es: " + resta);
+		
+		double resta2 = num2 - num1;
+		resta2 = Math.round(resta2 * r) / r;
+		
+		System.out.println("La resta de "+ num2 + "-" + num1+ " es: " + resta2);
+	}
+	
+	/**
+	 * 
+	 * @param num1 : PRIMER NUMERO INTRODUCIDO POR CONSOLA
+	 * @param num2 : SEGUNDO NUMERO INTRODUCIDO POR CONSOLA
+	 */
+	public void divisionNum(double num1, double num2)
+	{
+		//Division de ambos numeros
+		double div = num1 / num2;
+		div = Math.round(div * r) / r;
+		
+		System.out.println("La división de "+ num1 + "/" + num2+ " es: " + div);
+		
+		double div2 = num2 / num1;
+		div2 = Math.round(div2 * r) / r;
+		
+		System.out.println("La división de "+ num2 + "/" + num1+ " es: " + div2);
+	}
+	
+	/**
+	 * 
+	 * @param num1 : PRIMER NUMERO INTRODUCIDO POR CONSOLA
+	 * @param num2 : SEGUNDO NUMERO INTRODUCIDO POR CONSOLA
+	 */
+	public void multiplicacionnNum(double num1, double num2)
+	{
+		//Multilplicacion de los números
+		double mult = num1 * num2;
+		mult = Math.round(mult * r) / r;
+		
+		System.out.println("La multiplicación de ambos números es: " + mult);
+	}
+	
+	/**
+	 * 
+	 * @param num1 : PRIMER NUMERO INTRODUCIDO POR CONSOLA
+	 * @param num2 : SEGUNDO NUMERO INTRODUCIDO POR CONSOLA
+	 */
+	public void moduloNum(double num1, double num2)
+	{
+		//Modulo de los numeros
+		double mod = num1 % num2;
+		mod = Math.round(mod * r) / r;
+		
+		System.out.println("El modulo de ambos números es: " + mod);
+	}
+	
+	/**
+	 * 
+	 * @param num1 : PRIMER NUMERO INTRODUCIDO POR CONSOLA
+	 * @param num2 : SEGUNDO NUMERO INTRODUCIDO POR CONSOLA
+	 */
+	public void mayorMenor(double num1, double num2)
+	{
+		//Comprueba si cual es mayor y cual menor o si son iguales
+		if(num1 > num2)
+			System.out.println("El número " + num1 + " es mayor que " +  num2);
+		else if(num1 < num2)
+			System.out.println("El número " + num2 + " es mayor que " +  num1);
+		else
+			System.out.println("Los numeros son iguales");
+	}
 }
